f1eb70303ddff208a91064c7e7656deb2d61c4c8
Prayogi Indra <prayogi.indra2@gmail.com>
2019-06-25 09:25:44 +0700

    second commit

---
+++ src/main/java/com/xsis/TechnologyApplication.java
@@ -0,0 +1,19 @@
+package com.xsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class TechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String Home() {
+		return "Home";
+	}
+}
---
+++ src/main/java/com/xsis/controller/HomeController.java
@@ -0,0 +1,15 @@
+/*package com.xsis.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class HomeController {
+
+	@RequestMapping("/") 
+	public String home() {
+		return "home";
+	}
+}*/
